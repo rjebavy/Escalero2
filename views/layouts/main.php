ae6b2d5ae4a2b7fc2f33c9efe24d6b962243c5fb
Reinhard Jebavy <r.jebavy@gmx.at>
2018-01-10 10:42:13 +0100

    Hauptansicht Navigationsbalken geändert.
    brandlabel nur "Spielmal".

--- views/layouts/main.php
+++ views/layouts/main.php
@@ -28,7 +28,7 @@ AppAsset::register($this);
 <div class="wrap">
     <?php
     NavBar::begin([
-        'brandLabel' => 'Spielmal Escalero',
+        'brandLabel' => 'Spielmal',
         'brandUrl' => Yii::$app->homeUrl,
         'options' => [
             'class' => 'navbar-inverse navbar-fixed-top',
