22738ae96d0ff474fe1b23be3ff84dc673f06074
Reinhard Jebavy <r.jebavy@gmx.at>
2018-01-10 10:11:20 +0100

    Yii2 Hauptansicht angepasst.
    views/layouts/main.php; brandLabel & footer geändert.

--- views/layouts/main.php
+++ views/layouts/main.php
@@ -28,7 +28,7 @@ AppAsset::register($this);
 <div class="wrap">
     <?php
     NavBar::begin([
-        'brandLabel' => 'Fahrmalweg',
+        'brandLabel' => 'Spielmal Escalero',
         'brandUrl' => Yii::$app->homeUrl,
         'options' => [
             'class' => 'navbar-inverse navbar-fixed-top',
@@ -67,7 +67,7 @@ AppAsset::register($this);
 
 <footer class="footer">
     <div class="container">
-        <p class="pull-left">&copy; Reisebüro Lorbesch e. U. <?= date('Y') ?></p>
+        <p class="pull-left">&copy; Reinhard R. Jebavy <?= date('Y') ?></p>
 
         <p class="pull-right"><?= Yii::powered() ?></p>
     </div>
