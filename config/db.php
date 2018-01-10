30a45d3e728582f798a5f31b7e448a3d2e19048c
Reinhard Jebavy <r.jebavy@gmx.at>
2018-01-10 10:04:32 +0100

    Yii2 Zieldatenbankschema in config/db.php geändert.
    Vorher in phpMyAdmin neues Schema yii2escalero angelegt.

--- config/db.php
+++ config/db.php
@@ -2,7 +2,7 @@
 
 return [
     'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=localhost;dbname=yii2basic',
+    'dsn' => 'mysql:host=localhost;dbname=yii2escalero',
     'username' => 'root',
     'password' => 'db123!!!',
     'charset' => 'utf8',
