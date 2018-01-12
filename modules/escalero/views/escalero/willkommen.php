dc47907abf5a278bf5f4068532532cb0573c947d
Reinhard Jebavy <r.jebavy@gmx.at>
2018-01-12 01:37:15 +0100

    Gii: Controller für Escalero erfolgreich neu generiert, mit korrektem View-Pfad.
    Auch Screenshot des Ergebnisses. Aktionen: index, willkommen & spieler-anmelden.

---
+++ modules/escalero/views/escalero/willkommen.php
@@ -0,0 +1,13 @@
+<?php
+/* @var $this yii\web\View */
+?>
+<h1>escalero/willkommen</h1>
+    <p>
+        This is the view content for action "<?= $this->context->action->id ?>".
+        The action belongs to the controller "<?= get_class($this->context) ?>"
+        in the "<?= $this->context->module->id ?>" module.
+    </p>
+<p>
+    You may change the content of this page by modifying
+    the file <code><?= __FILE__; ?></code>.
+</p>
