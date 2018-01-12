ec753f665b3253da83907503f578f3fb8adcb298
Reinhard Jebavy <r.jebavy@gmx.at>
2018-01-12 15:55:09 +0100

    EscaleroController für's 'Anmelden' kodiert; Fehler bzgl. <namespace> korrigiert.
    Nach erfolgreichem Test, die dazu passende URL als Textfile ins /doc "geloggt".

--- modules/escalero/controllers/EscaleroController.php
+++ modules/escalero/controllers/EscaleroController.php
@@ -2,17 +2,38 @@
 
 namespace app\modules\escalero\controllers;
 
-class EscaleroController extends \yii\web\Controller
+use Yii;
+use yii\web\Controller;
+use app\modules\escalero\models\AnmeldenForm;
+
+
+class EscaleroController extends Controller
 {
     public function actionIndex()
     {
         return $this->render('index');
     }
 
-    public function actionSpielerAnmelden()
+    /**
+     * Anmelden action.
+     *
+     * @return Response|string
+     */
+    public function actionAnmelden()
     {
-        return $this->render('spieler-anmelden');
+        if (!Yii::$app->user->isGuest) {
+            return $this->goHome();
+        }
+
+        $model = new AnmeldenForm();
+        if ($model->load(Yii::$app->request->post()) && $model->anmelden()) {
+            return $this->goBack();
+        }
+        return $this->render('anmelden', [
+            'model' => $model,
+        ]);
     }
+	
 
     public function actionWillkommen()
     {
