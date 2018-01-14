4ee0b1b1c29de17471f17fc8517868a8e5208a19
Reinhard Jebavy <r.jebavy@gmx.at>
2018-01-15 00:42:19 +0100

    Vor Generierung der Menükomponente Admin, die Escalero Indexseite gestaltet, analog Site-index.
    FMsync C-Mirror & 32GB Stick; 00:41.

--- modules/escalero/views/escalero/index.php
+++ modules/escalero/views/escalero/index.php
@@ -1,9 +1,53 @@
 <?php
-/* @var $this yii\web\View */
-?>
-<h1>escalero/index</h1>
 
-<p>
-    You may change the content of this page by modifying
-    the file <code><?= __FILE__; ?></code>.
-</p>
+/* @var $this yii\web\View */
+
+$this->title = 'Escalero Startseite';
+?>
+
+<div class="site-index">
+
+    <div class="jumbotron">
+        <h1>Escalero Startseite</h1>
+
+        <p class="lead">Wähle untenstehende Aktionskomponente.</p>
+
+    </div>
+
+    <div class="body-content">
+
+        <div class="row">
+		    <div class="col-lg-4">
+                <h2>Escalero administrieren</h2>
+
+                <p>Spielgeschichte einsehen: <br> Logs checken, archivieren, ausmisten; Statistik, diverse Auswertungen und Berichte. <br> 
+				Datenpflege: <br> Stammdatenverwaltung; Spielverlaufstabellen checken,archivieren, ausmisten; Datenbank Logs checken, archivieren, 
+				ausmisten; Datenbankstatistik, diverse Auswertungen und Berichte. <br> 
+				Benutzerverwaltung. </p>
+
+                <p><a class="btn btn-default" href="http://localhost/03_Escalero2/web/index.php?r=escalero%2Fadmin%2Findex">Escalero Admin &raquo;</a></p>
+            </div>
+            <div class="col-lg-4">
+                <h2>Irgendwas 2</h2>
+
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et
+                    dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip
+                    ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu
+                    fugiat nulla pariatur.</p>
+
+                <p><a class="btn btn-default" href="#">Irgendwas 2 &raquo;</a></p>
+            </div>
+            <div class="col-lg-4">
+                <h2>Irgendwas 3</h2>
+
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et
+                    dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip
+                    ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu
+                    fugiat nulla pariatur.</p>
+
+                <p><a class="btn btn-default" href="#">Irgendwas 3 &raquo;</a></p>
+        </div>
+
+    </div>
+</div>
+
