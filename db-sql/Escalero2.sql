a6bea336572fdde4a9eb9ae76417d02130886ce5
Reinhard Jebavy <r.jebavy@gmx.at>
2018-01-14 13:23:42 +0100

    12:00 - 12:30:  MySQL Workbench; Tabellen 'wuerfelwerte' & 'wuerfeltableau' anlegen; SQL-script in /db-sql.

--- db-sql/Escalero2.sql
+++ db-sql/Escalero2.sql
@@ -28,6 +28,33 @@ CREATE TABLE IF NOT EXISTS `yii2escalero`.`spieler` (
 ENGINE = InnoDB;
 
 
+-- -----------------------------------------------------
+-- Table `yii2escalero`.`wuerfelwerte`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `yii2escalero`.`wuerfelwerte` (
+  `wuerfelwert` INT NOT NULL AUTO_INCREMENT,
+  `wuerfelbild` VARCHAR(2) NULL,
+  `bedeutung` VARCHAR(10) NULL,
+  PRIMARY KEY (`wuerfelwert`),
+  UNIQUE INDEX `wuerfelwert_UNIQUE` (`wuerfelwert` ASC),
+  UNIQUE INDEX `wuerfelbild_UNIQUE` (`wuerfelbild` ASC),
+  UNIQUE INDEX `bedeutung_UNIQUE` (`bedeutung` ASC))
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table `yii2escalero`.`wuerfeltableau`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `yii2escalero`.`wuerfeltableau` (
+  `wuerfel` INT NOT NULL AUTO_INCREMENT,
+  `wert` INT NULL,
+  `bild` VARCHAR(2) NULL,
+  `halten` TINYINT(1) NULL DEFAULT 0,
+  PRIMARY KEY (`wuerfel`),
+  UNIQUE INDEX `wuerfel_UNIQUE` (`wuerfel` ASC))
+ENGINE = InnoDB;
+
+
 SET SQL_MODE=@OLD_SQL_MODE;
 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS;
 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS;
